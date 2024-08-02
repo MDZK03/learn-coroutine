0c9201aea776b9da934fdc27c6884ba13d54771f
MDZK03 <127075461+MDZK03@users.noreply.github.com>
2024-08-02 16:37:10 +0700

    update ViewModel in usecase6

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase6/RetryNetworkRequestViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase6/RetryNetworkRequestViewModel.kt
@@ -1,14 +1,40 @@
 package com.lukaslechner.coroutineusecasesonandroid.usecases.coroutines.usecase6
 
+import androidx.lifecycle.viewModelScope
 import com.lukaslechner.coroutineusecasesonandroid.base.BaseViewModel
 import com.lukaslechner.coroutineusecasesonandroid.mock.MockApi
+import kotlinx.coroutines.launch
+import timber.log.Timber
 
 class RetryNetworkRequestViewModel(
     private val api: MockApi = mockApi()
 ) : BaseViewModel<UiState>() {
 
     fun performNetworkRequest() {
+        uiState.value = UiState.Loading //show loading
 
+        viewModelScope.launch {
+            val numberOfRetries = 2
+            try {
+                repeat(numberOfRetries) {
+                    try {
+                        loadAndroidVersion()
+                        return@launch
+                    } catch (exception: Exception) {
+                        Timber.e(exception) // log the exception
+                    }
+                }
+                loadAndroidVersion()
+            } catch (exception: Exception) {
+                Timber.e(exception) // log the exception
+                uiState.value = UiState.Error("Network request failed.") //toast msg to show error
+            }
+        }
+    }
+
+    private suspend fun loadAndroidVersion() {
+        val recentAndroidVersions = api.getRecentAndroidVersions()
+        uiState.value = UiState.Success(recentAndroidVersions) //show result
     }
 
 }
