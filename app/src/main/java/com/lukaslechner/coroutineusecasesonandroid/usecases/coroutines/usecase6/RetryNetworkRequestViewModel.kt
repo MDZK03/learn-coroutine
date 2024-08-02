5576e4f800b6dcc4c81653c5cc542119f2af9047
MDZK03 <127075461+MDZK03@users.noreply.github.com>
2024-08-02 17:14:30 +0700

    update ViewModel of usecase6 in coroutines
    the higher-order func retry now has exponential backoff

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase6/RetryNetworkRequestViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase6/RetryNetworkRequestViewModel.kt
@@ -3,6 +3,7 @@ package com.lukaslechner.coroutineusecasesonandroid.usecases.coroutines.usecase6
 import androidx.lifecycle.viewModelScope
 import com.lukaslechner.coroutineusecasesonandroid.base.BaseViewModel
 import com.lukaslechner.coroutineusecasesonandroid.mock.MockApi
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import timber.log.Timber
 
@@ -35,13 +36,21 @@ class RetryNetworkRequestViewModel(
         }
     }
 
-    private suspend fun <T> retry(numOfRetries: Int, block: suspend ()-> T): T {
+    private suspend fun <T> retry(
+        numOfRetries: Int,
+        initialDelayMilisec: Long = 100,
+        maxDelayMilisec: Long = 3000,
+        factor: Double = 2.5, //after each retry attempt, * factor
+        block: suspend ()-> T): T {
+        var currentDelay = initialDelayMilisec
         repeat(numOfRetries) {
             try {
                 return block()
             } catch (exception: Exception) {
                 Timber.e(exception)
             }
+            delay(currentDelay)
+            currentDelay = (currentDelay*factor).toLong().coerceAtMost(maxDelayMilisec)
         }
         return block()
     }
