d52635fd0dbc814e68db7d9334840a40c896933e
MDZK03 <127075461+MDZK03@users.noreply.github.com>
2024-08-02 14:57:49 +0700

    update usecase1

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase1/PerformSingleNetworkRequestViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase1/PerformSingleNetworkRequestViewModel.kt
@@ -1,18 +1,26 @@
 package com.lukaslechner.coroutineusecasesonandroid.usecases.coroutines.usecase1
 
+import androidx.lifecycle.viewModelScope
 import com.lukaslechner.coroutineusecasesonandroid.base.BaseViewModel
 import com.lukaslechner.coroutineusecasesonandroid.mock.MockApi
+import kotlinx.coroutines.launch
+import timber.log.Timber
 
 class PerformSingleNetworkRequestViewModel(
     private val mockApi: MockApi = mockApi()
 ) : BaseViewModel<UiState>() {
 
     fun performSingleNetworkRequest() {
-        uiState.value = UiState.Loading
+        uiState.value = UiState.Loading //show loading
 
         viewModelScope.launch {
-            val recentAndroidVersions = mockApi.getRecentAndroidVersions()
-            uiState.value = UiState.Success(recentAndroidVersions)
+            try {
+                val recentAndroidVersions = mockApi.getRecentAndroidVersions()
+                uiState.value = UiState.Success(recentAndroidVersions) //show result
+            } catch (exception: Exception) {
+                Timber.e(exception) // log the exception
+                uiState.value = UiState.Error("Network request failed.") //toast msg to show error
+            }
         }
     }
 }
