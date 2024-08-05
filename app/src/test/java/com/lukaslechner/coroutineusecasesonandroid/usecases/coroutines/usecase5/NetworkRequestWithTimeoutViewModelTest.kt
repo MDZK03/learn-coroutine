7f5644d90b6b4d024ed32bbd894232d72442e332
MDZK03 <127075461+MDZK03@users.noreply.github.com>
2024-08-05 14:59:21 +0700

    Create ViewModel test uc5

---
+++ app/src/test/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase5/NetworkRequestWithTimeoutViewModelTest.kt
@@ -0,0 +1,101 @@
+package com.lukaslechner.coroutineusecasesonandroid.usecases.coroutines.usecase5
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.lukaslechner.coroutineusecasesonandroid.mock.mockAndroidVersions
+import com.lukaslechner.coroutineusecasesonandroid.utils.ReplaceMainDispatcherRule
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.advanceUntilIdle
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.TestRule
+
+@ExperimentalCoroutinesApi
+class NetworkRequestWithTimeoutViewModelTest {
+
+    @get:Rule
+    val testInstantTaskExecutorRule: TestRule = InstantTaskExecutorRule()
+
+    @get: Rule
+    val replaceMainDispatcherRule = ReplaceMainDispatcherRule()
+
+    private val receivedUiStates = mutableListOf<UiState>()
+
+    @Test
+    fun `uc5 return Success on successful request within timeout`() = runTest {
+            val responseDelay = 1000L
+            val timeout = 1001L
+            val fakeApi = FakeSuccessApi(responseDelay)
+            val viewModel = NetworkRequestWithTimeoutViewModel(fakeApi)
+            viewModel.observe()
+
+            Assert.assertTrue(receivedUiStates.isEmpty())
+
+            viewModel.performNetworkRequest(timeout)
+
+            advanceUntilIdle()
+
+            Assert.assertEquals(
+                listOf(
+                    UiState.Loading,
+                    UiState.Success(mockAndroidVersions)
+                ),
+                receivedUiStates
+            )
+        }
+
+    @Test
+    fun `uc5 return Error with timeout error message if exceeded`() = runTest {
+            val responseDelay = 1000L
+            val timeout = 999L
+            val fakeApi = FakeSuccessApi(responseDelay)
+            val viewModel = NetworkRequestWithTimeoutViewModel(fakeApi)
+            viewModel.observe()
+
+            Assert.assertTrue(receivedUiStates.isEmpty())
+
+            viewModel.performNetworkRequest(timeout)
+
+            advanceUntilIdle()
+
+            Assert.assertEquals(
+                listOf(
+                    UiState.Loading,
+                    UiState.Error("Network request timed out.")
+                ),
+                receivedUiStates
+            )
+        }
+
+    @Test
+    fun `return Error on unsuccessful network response`() = runTest {
+            val responseDelay = 1000L
+            val timeout = 1001L
+            val fakeApi = FakeVersionsErrorApi(responseDelay)
+            val viewModel = NetworkRequestWithTimeoutViewModel(fakeApi)
+            viewModel.observe()
+
+            Assert.assertTrue(receivedUiStates.isEmpty())
+
+            viewModel.performNetworkRequest(timeout)
+
+            advanceUntilIdle()
+
+            Assert.assertEquals(
+                listOf(
+                    UiState.Loading,
+                    UiState.Error("Network request failed!")
+                ),
+                receivedUiStates
+            )
+        }
+
+    private fun NetworkRequestWithTimeoutViewModel.observe() {
+        uiState().observeForever { uiState ->
+            if (uiState != null) {
+                receivedUiStates.add(uiState)
+            }
+        }
+    }
+}
