e5285a8e61b417df2dff9c5f044e207274726073
MDZK03 <127075461+MDZK03@users.noreply.github.com>
2024-08-05 09:42:05 +0700

    Update ViewModel usecase12

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase12/CalculationInSeveralCoroutinesViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase12/CalculationInSeveralCoroutinesViewModel.kt
@@ -1,8 +1,11 @@
 package com.lukaslechner.coroutineusecasesonandroid.usecases.coroutines.usecase12
 
+import androidx.lifecycle.viewModelScope
 import com.lukaslechner.coroutineusecasesonandroid.base.BaseViewModel
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import java.math.BigInteger
 import kotlin.system.measureTimeMillis
 
@@ -11,30 +14,27 @@ class CalculationInSeveralCoroutinesViewModel(
     private val defaultDispatcher: CoroutineDispatcher = Dispatchers.Default
 ) : BaseViewModel<UiState>() {
 
-    fun performCalculation(
-        factorialOf: Int,
-        numberOfCoroutines: Int
-    ) {
+    fun performCalculation(factorialOf: Int, numberOfCoroutines: Int) {
         uiState.value = UiState.Loading
 
-        var factorialResult = BigInteger.ZERO
-        val computationDuration = measureTimeMillis {
-            factorialResult =
-                factorialCalculator.calculateFactorial(
-                    factorialOf,
-                    numberOfCoroutines
-                )
-        }
+        viewModelScope.launch {
+            var factorialResult = BigInteger.ZERO
+            val computationDuration = measureTimeMillis {
+                factorialResult =
+                    factorialCalculator.calculateFactorial(factorialOf, numberOfCoroutines)
+            }
 
-        var resultString = ""
-        val stringConversionDuration = measureTimeMillis {
-            resultString = convertToString(factorialResult)
-        }
+            var resultString = ""
+            val stringConversionDuration = measureTimeMillis {
+                resultString = convertToString(factorialResult)
+            }
 
-        uiState.value =
-            UiState.Success(resultString, computationDuration, stringConversionDuration)
+            uiState.value =
+                UiState.Success(resultString, computationDuration, stringConversionDuration)
+        }
     }
 
-    // TODO: execute on background thread
-    private fun convertToString(number: BigInteger): String = number.toString()
+    //execute on background thread
+    private suspend fun convertToString(number: BigInteger)
+    : String = withContext(defaultDispatcher) { number.toString() }
 }
