f4941c135d1ba098e9b99c6dc037ac1aad5f2e34
MDZK03 <127075461+MDZK03@users.noreply.github.com>
2024-08-06 16:42:46 +0700

    Add files via upload

---
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/playground/testsource/UserState.kt
@@ -0,0 +1,40 @@
+package com.lukaslechner.coroutineusecasesonandroid.playground.testsource
+
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+
+
+class FakeUserRepository : UserRepository() {
+    private var users = listOf<String>()
+
+    override suspend fun getAllUsers(): List<String> = users
+
+    override suspend fun register(name: String) {
+        delay(100L) // Simulate work
+        users = users + name
+        println("Registered $name")
+    }
+}
+
+
+class UserState(
+    private val userRepository: UserRepository,
+    private val scope: CoroutineScope,
+) {
+    private val _users = MutableStateFlow(emptyList<String>())
+    val users: StateFlow<List<String>> = _users.asStateFlow()
+
+    fun registerUser(name: String) {
+        scope.launch {
+            userRepository.register(name)
+            _users.update { userRepository.getAllUsers() }
+        }
+    }
+}
+// [END android_coroutine_test_user_state]
