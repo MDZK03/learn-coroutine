73db92dcd8ea5a6655a2a7e0aa80b384bfb858c5
MDZK03 <127075461+MDZK03@users.noreply.github.com>
2024-08-07 14:22:09 +0700

    clean-up ViewModel uc3 coroutine

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase3/PerformNetworkRequestsConcurrentlyViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase3/PerformNetworkRequestsConcurrentlyViewModel.kt
@@ -3,6 +3,7 @@ package com.lukaslechner.coroutineusecasesonandroid.usecases.coroutines.usecase3
 import androidx.lifecycle.viewModelScope
 import com.lukaslechner.coroutineusecasesonandroid.base.BaseViewModel
 import com.lukaslechner.coroutineusecasesonandroid.mock.MockApi
+import com.lukaslechner.coroutineusecasesonandroid.mock.VersionFeatures
 import kotlinx.coroutines.async
 import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.launch
@@ -14,13 +15,11 @@ class PerformNetworkRequestsConcurrentlyViewModel(
 
     fun performNetworkRequestsSequentially() {
         uiState.value = UiState.Loading
+
         viewModelScope.launch {
             try {
-                val oreoFeatures = mockApi.getAndroidVersionFeatures(27)
-                val pieFeatures = mockApi.getAndroidVersionFeatures(28)
-                val android10Features = mockApi.getAndroidVersionFeatures(29)
-
-                val versionsFeaturesList = listOf(oreoFeatures, pieFeatures, android10Features)
+                val versionsFeaturesList =
+                    listOf(getFeatures(27), getFeatures(28), getFeatures(29))
                 uiState.value = UiState.Success(versionsFeaturesList)
             } catch (exception: Exception) {
                 uiState.value = UiState.Error("Network request failed.")
@@ -31,17 +30,21 @@ class PerformNetworkRequestsConcurrentlyViewModel(
     fun performNetworkRequestsConcurrently() = runBlocking {
         uiState.value = UiState.Loading
 
-        val oreoFeatures = viewModelScope.async { mockApi.getAndroidVersionFeatures(27) }
-        val pieFeatures = viewModelScope.async { mockApi.getAndroidVersionFeatures(28) }
-        val android10Features = viewModelScope.async { mockApi.getAndroidVersionFeatures(29) }
-
         viewModelScope.launch {
             try {
-                val versionFeaturesList = awaitAll(oreoFeatures, pieFeatures, android10Features)
+                val versionFeaturesList = awaitAll(
+                    async { getFeatures(27) },
+                    async { getFeatures(28) },
+                    async { getFeatures(29) }
+                )
                 uiState.value = UiState.Success(versionFeaturesList)
             } catch (exception: Exception) {
                 uiState.value = UiState.Error("Network request failed.")
             }
         }
+    }
+
+    private suspend fun getFeatures(apiLevel: Int): VersionFeatures {
+        return mockApi.getAndroidVersionFeatures(apiLevel)
     }
 }
